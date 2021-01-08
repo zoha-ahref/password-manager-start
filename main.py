aeb3ba5918cae28098a9c178dd74980faca4f211
zoha <sweetangelzoha@gmail.com>
2021-01-08 07:24:42 -0500

    files committed: error handling included

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from tkinter import *
 from tkinter import messagebox
 import pyperclip
-
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 import random
-def generate():
 
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+def generate():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -26,28 +29,65 @@ def generate():
     pyperclip.copy("The text to be copied to clipboard")
 
 
-
-
-
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+# website = web_input.get()
+# email = email_input.get()
+# password = pass_input.get()
+
+
 def save():
     website = web_input.get()
     email = email_input.get()
     password = pass_input.get()
+
+    new_data = {website: {
+        "email": email,
+        "password": password,
+    }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Message", message="Please don't leave the fields empty")
 
 
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered:\n "
-                                                              f"Email: {email}\n Password: {password}\n Is it ok to save? ")
+        try:
+            with open("data.json", "r") as f:
+                data = json.load(f)
 
-        if is_ok:
-            with open("data.txt", "a") as f:
-                f.write(f"{website} | {email} | {password}\n")
+        except FileNotFoundError:
+            with open("data.json", "w") as f:
+                json.dump(new_data, f, indent=4)
+        else:
+
+            data.update(new_data)
+            with open("data.json", "w") as f:
+                json.dump(data, f, indent=4)
+
+        finally:
             web_input.delete(0, END)
             pass_input.delete(0, END)
+
+
+def find_password():
+    website = web_input.get()
+
+    try:
+        with open("data.json", "r") as f:
+            data = json.load(f)
+
+    except FileNotFoundError:
+        messagebox.showinfo(title="error", message="no file exists")
+
+
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+
+            messagebox.showinfo(title=website, message=f"Email: {email}\n Password: {password}")
+        else:
+            messagebox.showinfo(title="error", message="no website found")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -63,8 +103,8 @@ canvas.grid(column=1, row=0)
 web_label = Label(text="Website: ")
 web_label.grid(column=0, row=1)
 
-web_input = Entry(width=35)
-web_input.grid(column=1, row=1, columnspan=2)
+web_input = Entry(width=21)
+web_input.grid(column=1, row=1)
 web_input.focus()
 
 email_label = Label(text="Email/Username: ")
@@ -79,6 +119,9 @@ pass_label.grid(column=0, row=3)
 
 pass_input = Entry(width=21)
 pass_input.grid(column=1, row=3)
+
+button_search = Button(text="Search", command=find_password)
+button_search.grid(column=2, row=1)
 
 button_gen_pass = Button(text="Generate Password", command=generate)
 button_gen_pass.grid(column=2, row=3)
